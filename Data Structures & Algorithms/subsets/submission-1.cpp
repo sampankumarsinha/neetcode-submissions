ac6f0e98acd83dda77c0a6883bdb53511a1e0cba
SAMPAN KUMAR SINHA <sinhay610@gmail.com>
2026-07-19 00:44:10 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+vector<vector<int>>res;
+  void solve(int i,vector<int>& nums,vector<int>&temp){
+      if(i==nums.size()){
+        res.push_back(temp);
+        return;
+      }
+      temp.push_back(nums[i]);
+      solve(i+1,nums,temp);
+      temp.pop_back();
+      solve(i+1,nums,temp);
+      
+  }
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+      vector<int>temp;
+      solve(0,nums,temp);
+      return res;
+    }
+};
+        
